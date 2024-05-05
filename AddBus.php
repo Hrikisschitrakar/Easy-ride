87c7c99ace94a5ea41520e001388da73376801f3
Pramdip Shrestha <119594157+Pramdip@users.noreply.github.com>
2024-05-05 08:51:13 +0545

    Added file to add buses for managing bus

---
+++ AddBus.php
@@ -0,0 +1,169 @@
+<?php 
+
+    session_start();
+
+?>
+<?php include("connection.php")?>
+<!--
+<!DOCTYPE html>
+<html>
+<head>
+    <title>admin Panel suraksha</title>
+</head>
+<body>
+
+   <?php// echo "welcome:".  $_SESSION['id']; ?>
+   <a href="adminLogout.php"><button class="btnHome">logout</button></a>
+
+</body>
+</html>
+
+-->
+
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Routes adding</title>
+  <!--cdn icon library -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="sidebar.css">
+<link rel="stylesheet" href="signUp.css">
+<link rel="stylesheet" href="AddBus.css">
+    
+</head>
+<body>
+  <input type="checkbox" id="check">
+
+  <label for="check">
+<i class="fa fa-bars" id="btn"></i>
+<i class="fa fa-times" id="cancle"></i>
+
+
+  </label>
+  <div class="sidebar">
+<header><img src="adminprofile.jpg">
+<p><?php echo $_SESSION['username']; ?></p>
+</header>
+<ul>
+
+
+     <li><a href="admin_dashboard.php">Manage Routes</a></li>
+     <li><a href="manageprofiles.php">Manage Profiles</a></li>
+    <li><a href="ManagesBuses.php">Manage Buses</a></li>
+    <li><a href="BookingManage.php">Booking People</a></li>
+    <li><a href="PaymentManage.php">Transaction</a></li>
+    <li><a href="loginMenu.php">logout</a></li>
+  <!--  <li><a href="#">Event</a></li>
+    <li><a href="#">About</a></li>
+    <li><a href="#">Service</a></li>
+    <li><a href="#">Contact</a></li>-->
+    </ul>
+ <!--  <li>
+      <div class="social-links">
+        <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
+        <a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
+        <a href="#" class="instagram"><i class="fa fa-instagram"></i></a>
+        <a href="#" class="google-plus"><i class="fa fa-youtube"></i></a>
+        
+      </div>
+    </li>-->
+   
+
+</div>
+
+
+
+<div class="sidebar2">
+
+
+        <?php 
+
+
+  if(isset($_POST['AddBus'])){
+
+     
+       //$id=$_POST['id'];
+       $nameOFbus=$_POST['bus_name'];
+       $tel=$_POST['tel'];
+       
+    
+
+    
+
+
+       if($conn->connect_error)
+          {
+            die('Connection Failed :'.$conn->connect_error);
+          }
+          else
+          {
+
+
+              $stmt=$conn->prepare("INSERT INTO bus(Bus_Name,Tel) VALUES(?,?)");
+              //table3 is the table name//
+
+              $stmt->bind_param("ss", $nameOFbus,$tel);
+
+              $stmt->execute();
+              
+                                 echo ("<script LANGUAGE='JavaScript'>
+                      window.alert('Succesfully Bus Added!!!');
+                      window.location.href='ManagesBuses.php';
+                      </script>");
+               
+
+
+              
+              $stmt->close();
+              $conn->close();
+              }
+                
+          
+      }     
+  
+
+   ?>
+
+
+
+
+          <div class="wrapper">
+  <div class="registration_form">
+    <div class="title">
+      Bus adding
+    </div>
+
+    <form action="#" method="POST">
+      <div class="form_wrap">
+        
+        <div class="input_wrap">
+          <label for="title">Bus Name</label>
+          <input type="text" id="title" name="bus_name" placeholder="Bus Name" required>
+        </div>
+
+
+        <div class="input_wrap">
+          <label for="title">Telephone</label>
+          <input type="text" id="title" name="tel" placeholder="Tel" required>
+        </div>
+
+        <div class="input_wrap">
+          <input type="submit" value="Add Bus Now" class="submit_btn" name="AddBus">
+
+        </div>
+
+
+
+      </div>
+    </form>
+  </div>
+</div>
+
+
+
+
+</div>
+
+</body>
+</html>
