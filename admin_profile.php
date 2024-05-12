f93a635c727e92b81b388c09c85d8a4c70c08904
Pramdip Shrestha <119594157+Pramdip@users.noreply.github.com>
2024-05-12 10:36:16 +0545

    updated profile style

--- admin_profile.php
+++ admin_profile.php
@@ -17,31 +17,39 @@ session_start();
     <link rel="stylesheet" href="admin_profiles.css">
 </head>
 <body> 
-    <div class="user-greeting"><b><font color="#fff"> Hello !!! <?php echo $user_data_admin['username'];?></font></b></div>
-    <div class="content-wrapper">
-        <div class="left">
-            <img src="iconpic.jpg" alt="user" width="100" class="avatar">
-            <br>
-            <a href="admin_dashboard.php"><button class="btn home-btn">Home </button></a>
+    <div class="container">
+        <div class="user-greeting"><b><font color="#fff"> Hello !!! <?php echo $user_data_admin['username'];?></font></b></div>
+        <div class="content-wrapper">
+            <div class="middle-box">
+                <div class="left">
+                    <img src="iconpic.jpg" alt="user" width="100" class="avatar">
+                    <br>
+                    <a href="admin_dashboard.php"><button class="btn home-btn">Home </button></a>
+                </div>
+                <div class="right">
+                    <h2>Account Information</h2><hr/>
+                    <div class="info">
+                        <p><strong>User name:- <?php echo $user_data_admin['username'];?>   </p><br>
+                        <p><strong>Password:- <?php echo $user_data_admin['password'];?> </p>
+                    </div> 
+                   
+                    <h2>LOGOUT & SECURITY</h2><hr>
+                    <br>
+                    <div class='actions'>
+                        <a href="updateProfile.php?id=<?php echo $user_data_admin['id'];?>">
+                            <button class="btn update-btn">Update</button>
+                        </a>
+                        <a href="loginMenu.php">
+                            <button class="btn logout-btn">Logout</button>
+                        </a>
+                        <a href="deleteProfile.php?id=<?php echo $user_data_admin['id'];?>">
+                            <button class="btn delete-btn">Delete</button>
+                        </a>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-
-    <div class="right">
-        <h2>Account Information</h2><hr/>
-        <div class="info">
-        <p><strong>User name:- <?php echo $user_data_admin['username'];?>   </p><br>
-        <p><strong>Password:- <?php echo $user_data_admin['password'];?> </p></div> 
-       
-        
-        <h2>LOGOUT & SECURITY</h2><hr>
-        <br>
-        <div class='actions'>
-        <a href="updateProfile.php?id=<?php echo $user_data_admin['id'];?>">
-        <button class="btn update-btn">Update</button></a>
-        <a href="loginMenu.php">
-        <button class="btn logout-btn">Logout</button></a>
-        <a href="deleteProfile.php?id=<?php echo $user_data_admin['id'];?>">
-        <button class="btn delete-btn">Delete</button></a></div>
-    </div>
 </body>
+
 </html>
